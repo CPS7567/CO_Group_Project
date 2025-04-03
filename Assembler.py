1d1d5c1914c4396ca6cbb03f1b92ae8b1370ee35
Divyeh-IIITD <divyeh24205@iiitd.ac.in>
2025-04-03 16:10:06 +0530

    Update Assembler.py

--- Assembler.py
+++ Assembler.py
@@ -1,6 +1,6 @@
 import sys
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+input_file = "input.txt"
+output_file = "output.txt"
 
 
 abi_to_register = {"zero":"x0","ra":"x1","sp":"x2","gp":"x3","tp":"x4","t0":"x5","t1":"x6","t2":"x7","s0":"x8","fp":"x8","s1":"x9","a0":"x10","a1":"x11","a2":"x12","a3":"x13","a4":"x14","a5":"x15","a6":"x16","a7":"x17","s2":"x18","s3":"x19","s4":"x20","s5":"x21","s6":"x22","s7":"x23","s8":"x24","s9":"x25","s10":"x26","s11":"x27","t3":"x28","t4":"x29","t5":"x30","t6":"x31"}
@@ -10,6 +10,9 @@ registers = ['x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10', 
 r_dict = ['add','sub','slt','srl','or','and']
 i_dict = ['lw','addi','jalr']
 b_dict = ['beq','bne','blt']
+
+bonus = ['rst' , 'mul' , 'halt' , 'rvrs']
+
 
 
 def regis_to_bin(x):
@@ -225,6 +228,72 @@ lines = []
 line = file.readline()
 labels = {}
 i = 0
+
+def mul(a, l ,index):
+    l = l.split(',')
+    b,c,d = l[0],l[1],l[2]
+    k = {'funct7' : '0000000' , 'funct3' : '000' , 'opcode' : '1110111'}
+    if b in abi_to_register or b in registers:
+            b = abi_to_register[b] if b in abi_to_register else b
+            b = int(b[1:])
+            b ,i = regis_to_bin(b) 
+            b = '0'*(5-i) + b
+    else:
+            print (f"ERROR at line {index+1}")
+            return f"ERROR at line {index+1}"
+    if c in abi_to_register or c in registers:
+            c = abi_to_register[c] if c in abi_to_register else c
+            c = int(c[1:])
+            c ,i= regis_to_bin(c) 
+            c = '0'*(5-i) + c
+    else:
+            print (f"ERROR at line {index+1}")
+            return f"ERROR at line {index+1}"
+    if d in abi_to_register or d in registers:
+            d = abi_to_register[d] if d in abi_to_register else d
+            d = int(d[1:])
+            d , i = regis_to_bin(d)
+            d = '0'*(5-i) + d
+    else:
+            print (f"ERROR at line {index+1}")
+            return f"ERROR at line {index+1}"
+    t = f"{k['funct7']}{d}{c}{k['funct3']}{b}{k['opcode']}"
+
+    return t
+
+def rst():
+    t = "00000000000000000001000001110111"
+    return t 
+
+def halt():
+    t = "00000000000000000010000001110111"
+    return t
+
+def rvrs(a,l , index):
+    l = l.split(',')
+    b = l[0]   #rdddd
+    c = l[1] #rs11111
+    k = {'funct7' : '0000000' , 'funct3' : '011' , 'opcode' : '1110111'}
+    if b in abi_to_register or b in registers:
+            b = abi_to_register[b] if b in abi_to_register else b
+            b = int(b[1:])
+            b ,i = regis_to_bin(b) 
+            b = '0'*(5-i) + b
+    else:
+            print (f"ERROR at line {index+1}")
+            return f"ERROR at line {index+1}"
+    if c in abi_to_register or c in registers:
+            c = abi_to_register[c] if c in abi_to_register else c
+            c = int(c[1:])
+            c ,i= regis_to_bin(c) 
+            c = '0'*(5-i) + c
+    else:
+            print (f"ERROR at line {index+1}")
+            return f"ERROR at line {index+1}"
+
+    t = f"{k['funct7']}00000{c}{k['funct3']}{b}{k['opcode']}"
+    return t
+
 while line:
     if '\n' in line:
         line = line[:-1]
@@ -243,7 +312,10 @@ while line:
     lines.append(line)
     line = file.readline()
     i += 1
+
 file.close()
+
+print(lines)
 
 
 file = open(output_file,'w')
@@ -258,6 +330,15 @@ for i in lines:
         st = s_type(i[0],i[1],lines.index(i))
     elif i[0] == 'jal':
         st = j_type(i[0],i[1],lines.index(i))
+    elif i[0] == 'mul':
+        st = mul(i[0] , i[1] , lines.index(i))
+    elif i[0] == 'rst':
+        st = rst()
+    elif i[0] == 'rvrs':
+        rvrs(i[0],i[1] , lines.index(i))
+        pass
+    elif i[0] == "halt":
+        st = halt()
     else:
         st = f"ERROR at line {lines.index(i)+1}"
         print(f"ERROR at line {lines.index(i)+1}")
